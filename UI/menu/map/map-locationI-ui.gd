bcf461a0cca04e0c6ed025feec2b1e28a469fca2
Luca Noltenius <luca-nick@online.de>
2024-05-09 12:03:06 +0200

    Progress #10 (Backup #4)

--- UI/menu/map/map-locationI-ui.gd
+++ UI/menu/map/map-locationI-ui.gd
@@ -1,11 +1,63 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+@export var type: Enums.locationType
+@export var attributes: Array[Enums.locationAttribute]
+
+@onready var game: Node2D = get_tree().get_root().get_node("Game")
+@onready var typeContainer: TextureRect = get_node("VBoxContainer/PanelContainer/TextureRect")
+@onready var attributeContainer: HBoxContainer = get_node("VBoxContainer/HBoxContainer")
+
+
 func _ready():
-	pass # Replace with function body.
+	setType()
+	setAttributes()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func setType():
+	var texture: Texture
+	match type:
+		Enums.locationType.SETTLEMENT:
+			texture = preload("res://assets/UI/icons/locations/Settlement.png")
+		Enums.locationType.RUINS:
+			texture = preload("res://assets/UI/icons/locations/Ruins.png")
+		Enums.locationType.MYSTERY:
+			texture = preload("res://assets/UI/icons/locations/Mystery.png")
+		Enums.locationType.RETREAT:
+			texture = preload("res://assets/UI/icons/locations/Retreat.png")
+	typeContainer.texture = texture
+
+
+func setAttributes():
+	for attribute in attributes:
+		var attributeScene = preload("res://UI/menu/map/map-location-ui.tscn").instantiate()
+		attributeContainer.add_child(attributeScene)
+		var texture: Texture
+		match attribute:
+			Enums.locationAttribute.DARKNESS:
+				texture = preload("res://assets/UI/icons/locations/Darkness.png")
+			Enums.locationAttribute.EXPOSURE:
+				texture = preload("res://assets/UI/icons/locations/Exposure.png")
+			Enums.locationAttribute.INFESTATION:
+				texture = preload("res://assets/UI/icons/locations/Infestation.png")
+			Enums.locationAttribute.DANGER:
+				texture = preload("res://assets/UI/icons/locations/Danger.png")
+		attributeScene.setup(texture)
+
+
+func _on_panel_container_mouse_entered():
+	AnimationsS.setSize(self, 1.3, 0.15)
+
+
+func _on_panel_container_mouse_exited():
+	AnimationsS.setSize(self, 1, 0.15)
+
+
+func _on_button_pressed():
+	findPathway()
+	game.caveGenerator.iterations = 2
+	game.generateCave()
+
+
+func findPathway():
+	print(self.get_parent().get_children())
